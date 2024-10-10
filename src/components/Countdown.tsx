0c7f5a160f2f2addc9679c55c65a6c0d6c426187
davidtourek <dtourek@centrum.cz>
2024-10-10 17:11:21 +0200

    added text inside circle element

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,3 +1,5 @@
+import { useCounter } from "@/hooks/useCounter.tsx";
+import { displayTime } from "@/lib/time.ts";
 import { colorByPhase } from "@/lib/utils.ts";
 import { CounterPhase } from "@/types.ts";
 
@@ -13,11 +15,25 @@ export const Countdown = ({ secondsTotal, secondsRemaining, phase }: ICountdownP
   const strokeDashArray = 2 * Math.PI * radiusCount;
   const strokeDashoffset = strokeDashArray * ((100 - progressPercent) / 100);
   const width = 500;
+  const { counter } = useCounter();
 
   return (
     <>
       <svg width={width} height={width} viewBox={`0 0 ${width} ${width}`} style={{ transform: "rotate(-90deg)" }}>
-        <circle r={radiusCount} cx={width / 2} cy={width / 2} fill="transparent" stroke={colorByPhase[phase]} strokeWidth="5px"></circle>
+        <circle r={radiusCount} cx={width / 2} cy={width / 2} fill="none" stroke={colorByPhase[phase]} strokeWidth="5px" />
+        <text
+          x="50%"
+          y="50%"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fill="white"
+          fontSize="80px"
+          dy=".3em"
+          style={{ transform: "rotate(90deg)", transformOrigin: "center" }}
+        >
+          {displayTime(counter.timeLeft)}
+        </text>
+
         <circle
           r={radiusCount}
           cx={width / 2}
--- src/components/TimeDisplay.tsx
+++ src/components/TimeDisplay.tsx
@@ -1,17 +1,8 @@
 import { Countdown } from "@/components/Countdown.tsx";
 import { useCounter } from "@/hooks/useCounter.tsx";
 import { useInterval } from "@/hooks/useInterval.ts";
-import { displayTime, phaseSeconds } from "@/lib/time.ts";
-import { toSeconds } from "@/lib/utils.ts";
+import { phaseSeconds } from "@/lib/time.ts";
 import { CounterActions } from "@/types.ts";
-
-const DisplayTime = ({ timeLeft }: { timeLeft: number }) => {
-  const secondsLeft = toSeconds(timeLeft);
-  if (secondsLeft <= 10) {
-    return <div className={"flex justify-center p-4 text-6xl text-red-800"}>{displayTime(timeLeft)}</div>;
-  }
-  return <div className={"flex justify-center p-4 text-6xl"}>{displayTime(timeLeft)}</div>;
-};
 
 export const TimeDisplay = () => {
   const { counter, dispatch } = useCounter();
@@ -23,13 +14,10 @@ export const TimeDisplay = () => {
       <div className="flex justify-evenly">
         <div className="text-5xl">Rounds left {counter.rounds}</div>
       </div>
-      <div>
-        <Countdown secondsRemaining={counter.timeLeft / 100} secondsTotal={secondsTotalInPhase} phase={counter.phase} />
-      </div>
+      <Countdown secondsRemaining={counter.timeLeft / 100} secondsTotal={secondsTotalInPhase} phase={counter.phase} />
       <div className="flex justify-evenly">
         <div className="text-5xl">{counter.phase}</div>
       </div>
-      <DisplayTime timeLeft={counter.timeLeft} />
     </div>
   );
 };
