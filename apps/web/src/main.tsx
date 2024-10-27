ca3738aab8f4ef57c1e3e166ffa6969ed930a90f
davidtourek <dtourek@centrum.cz>
2024-10-27 20:32:26 +0100

    feat: added yarn monorepo, turborepo, mobile app with react-native and expo

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,13 @@
+import { Providers } from "@/Providers.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Providers>
+      <App />
+    </Providers>
+  </StrictMode>,
+);
