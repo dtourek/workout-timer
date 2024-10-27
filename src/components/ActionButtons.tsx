75333527051e191fdbef08de7b28db5fb6d4ba12
davidtourek <dtourek@centrum.cz>
2024-10-27 11:55:58 +0100

    feat: dark mode css adjusted

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -24,25 +24,32 @@ export const ActionButtons = () => {
     }
   }, [counter]);
 
-  const disabledClasses = (isDisabled: boolean) =>
-    isDisabled ? "cursor-not-allowed border-2  border-gray-300 text-gray-300 hover:text-gray-300 hover:bg-transparent" : "border-primary";
+  const disabledClasses = (isDisabled: boolean, isPrimaryButton?: boolean) => {
+    if (isDisabled && !isPrimaryButton) {
+      return "cursor-not-allowed text-gray-300 hover:text-gray-300 dark:text-gray-500 dark:hover:text-gray-500 dark:border:gray-300 dark:hover:text-gray-500 dark:hover:border-gray-300 dark:focus:ring-gray-300";
+    }
+    if (isDisabled && isPrimaryButton) {
+      return "cursor-not-allowed text-gray-300 hover:text-gray-300 dark:border:gray-300 dark:hover:border-gray-300 dark:focus:ring-gray-800 bg-opacity-50";
+    }
+    return "cursor-pointer";
+  };
 
   return (
     <div className="flex flex-row items-center">
       <div
-        className={`cursor-pointer rounded-full border-2 border-white bg-white p-5 text-center text-sm font-medium text-gray-500 shadow-md shadow-gray-300 hover:inline-flex hover:border-2 hover:border-gray-300 hover:text-gray-500 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-300 dark:text-blue-300 dark:shadow-muted dark:hover:bg-gray-500 dark:hover:text-white dark:focus:ring-blue-800`}
+        className={`cursor-pointer rounded-full bg-white p-5 text-center text-sm font-medium text-gray-500 shadow-md shadow-gray-300 hover:inline-flex hover:text-gray-500 hover:shadow-lg focus:outline-none focus:ring-4 dark:border-2 dark:border-white dark:bg-transparent dark:text-white dark:shadow-muted dark:hover:bg-background dark:hover:text-white`}
         onClick={handleReset}
       >
         <TimerResetIcon />
       </div>
       <div
-        className={`${disabledClasses(isCounterRunning)} mx-4 cursor-pointer items-center rounded-full border-2 bg-primary p-8 text-center text-sm font-medium text-primary text-white shadow-md shadow-gray-300 transition-all duration-75 ease-in hover:inline-flex hover:text-white hover:opacity-90 focus:outline-none focus:ring-4 dark:border-primary dark:bg-primary dark:text-white dark:shadow-muted dark:hover:text-white dark:focus:ring-blue-800`}
+        className={`mx-4 items-center rounded-full bg-primary p-8 text-center text-sm font-medium text-primary text-white shadow-md shadow-gray-300 transition-all duration-75 ease-in hover:inline-flex hover:text-white hover:opacity-90 hover:shadow-lg focus:outline-none focus:ring-4 dark:border-primary dark:bg-primary dark:text-white dark:shadow-muted dark:hover:text-white dark:focus:ring-blue-800 ${disabledClasses(isCounterRunning, true)}`}
         onClick={handlePlay}
       >
         <Play />
       </div>
       <div
-        className={`${disabledClasses(counter.status === CounterStatus.PAUSED)} cursor-pointer rounded-full border-2 border-white bg-white p-5 text-center text-sm font-medium text-gray-500 shadow-md shadow-gray-300 hover:inline-flex hover:border-2 hover:border-gray-300 hover:text-gray-500 hover:shadow-lg focus:outline-none focus:ring-4 dark:border-blue-300 dark:text-blue-300 dark:shadow-muted dark:hover:bg-gray-500 dark:hover:text-white dark:focus:ring-blue-800`}
+        className={`rounded-full bg-white p-5 text-center text-sm font-medium text-gray-500 shadow-md shadow-gray-300 hover:inline-flex hover:text-gray-500 hover:shadow-lg focus:outline-none focus:ring-4 dark:border-2 dark:border-white dark:bg-transparent dark:text-white dark:shadow-muted dark:hover:bg-background dark:hover:text-white ${disabledClasses(counter.status === CounterStatus.PAUSED)}`}
         onClick={handlePause}
       >
         <Pause />
