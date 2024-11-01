59bf3df91b79c99b571d8faa0bc01008a699317b
davidtourek <dtourek@centrum.cz>
2024-11-01 14:09:52 +0100

    wip - feat: web - added audi hook and mp3 sound

---
+++ apps/web/src/hooks/useAudio.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const useAudio = (url: string): [boolean, () => void] => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = (): void => setPlaying(!playing);
+
+  useEffect(() => {
+    if (playing) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }, [playing]);
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(false));
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
